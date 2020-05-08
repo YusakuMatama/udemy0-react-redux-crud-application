82b191bbfbe0434202951b771675424e39885bc8
yusaku-matama <yusaku.matama@mt-square.co.jp>
2020-05-08 09:59:16 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-function App() {
-  // const greeting = "Hi, Tom!";
-  // const dom = <h1 className="fooa">{greeting}</h1>;
-  return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked")}}/>
-      </React.Fragment>
-  );
+// function App() {
+//   // const greeting = "Hi, Tom!";
+//   // const dom = <h1 className="fooa">{greeting}</h1>;
+//   return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked")}}/>
+//       </React.Fragment>
+//   );
+// }
+
+const App = () => {
+    return (
+        <div>
+            {Cat()}
+            {Cat()}
+        </div>
+
+        )
 }
 
+const Cat = () => {
+    return <div>Meossssss</div>
+}
 
 export default App;
